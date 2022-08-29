af9e4dd6757182cd901a2de62b2085a6180d1661
Adeyinka Junaid <yinkajunn@gmail.com>
2022-08-29 18:27:39 +0100

    Added different webpages + header and navbar

---
+++ src/components/Navbar.js
@@ -0,0 +1,17 @@
+import React from 'react'
+  
+
+const Navbar = () => {
+  return (
+    <div>
+      <nav>
+        <a href="/Home Page(index).html">Home</a>
+        <a href="/Register Page.html"> Register </a>
+        <a href="/Calculator.html"> Calculator</a>
+        <a href="/Login Page.html">Login</a>
+      </nav>
+    </div>
+  )
+}
+
+export default Navbar
