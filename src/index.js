f53e50425f58df2f6a9212d53f7b97fbb8db17aa
Adeyinka Junaid <yinkajunn@gmail.com>
2022-09-19 18:46:14 +0100

    Oragnized components

--- src/index.js
+++ src/index.js
@@ -3,15 +3,12 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
-import Navbar from './components/Navbar';
-import Header from './components/Header';
+
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <Navbar/>
-    <Header name={"Yinka's Webpage"}/>
-    <App />
+    <App /> 
   </React.StrictMode>
 );
 
